bb4609e1eb647cbc6374abca71fe2166d2761f19
ferrante <brandon.f70@gmail.com>
2022-06-29 09:10:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typed', {
+    strings: ["Bonjour à tous et à toutes, je me présente je m'appelle Brandon. Après une carrière m'ayant fait découvrir plusieurs milieux professionels. J'ai décidé de me lancer à fond dans l'apprentissage du code, je viens d'obtenir ma certification de dév web et web mobile après 7 mois de formation. Plus qu'un avenir professionnel, je me suis découvert une passion, celle de coder."],
+    typeSpeed: 40,
+  });
+  
+
+
+
